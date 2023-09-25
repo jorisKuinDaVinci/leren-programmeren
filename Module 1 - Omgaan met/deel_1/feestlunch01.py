6832a0f89ac896e3b3cb37112e62470f58be85b0
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-09-25 15:35:37 +0200

    verder gewerkt

--- Module 1 - Omgaan met/deel_1/feestlunch01.py
+++ Module 1 - Omgaan met/deel_1/feestlunch01.py
@@ -1,4 +1,9 @@
 croissantjes = 0,39
 stokbroden = 2,78
+#totaal = 17 * croissantjes + 2 * stokbroden
 kortingsbonnen = 0,50
-print(17 * croissantjes + 2 * stokbroden - 3 * kortingsbonnen)
+totaal = 17 * croissantjes + 2 * stokbroden - 3 * kortingsbonnen
+if totaal > 0:
+    print(f"De totale kosten zijn: {totaal} euro")
+elif totaal == 18.88:
+    print(f'De feestlunch kost je bij de bakker 18.88 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!')
---
+++ Module 1 - Omgaan met/deel_1/speelhal01.py
@@ -0,0 +1,10 @@
+vier_vrienden = 7.45 * 4
+
+gameseat = 9 * 0.37
+
+totaal = vier_vrienden + gameseat
+#print(f"De totale kosten zijn: {totaal} euro")
+if totaal > 0:
+    print(f"De totale kosten zijn: {totaal} euro")
+elif totaal == 44.44:
+    print(f'Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 44.44 euro')
