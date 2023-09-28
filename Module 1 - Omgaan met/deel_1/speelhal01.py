4fe324ef5eede245e82e736678f74edd495234aa
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-09-28 12:25:37 +0200

    verder gewerkt

--- Module 1 - Omgaan met/deel_1/speelhal01.py
+++ Module 1 - Omgaan met/deel_1/speelhal01.py
@@ -2,11 +2,12 @@ VIER_VRIENDEN = 7.45 * 4
 PRIJS_PER_VIJF_MINUUT = 0.37
 AANTAL_MINUTEN = 45
 AANTAL_RONDJES = 9
-gameseat_kosten = AANTAL_RONDJES * PRIJS_PER_VIJF_MINUUT
+gameseat_kosten = PRIJS_PER_VIJF_MINUUT * AANTAL_RONDJES
 
 totaal = VIER_VRIENDEN + gameseat_kosten
 #print(f"De totale kosten zijn: {totaal} euro")
 if totaal > 0:
-    print(f"De totale kosten zijn: {totaal} euro")
+    #print(f"De totale kosten zijn: {totaal} euro")
+    print(f'Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar {totaal} euro')
 elif totaal == 44.44:
     print(f'Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 44.44 euro')
