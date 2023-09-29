22c4f306835ddb816f3b54d439750003a1dc8dfd
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-09-29 13:40:40 +0200

    verbeterd feestlunch en speelhal

--- Module 1 - Omgaan met/deel_1/feestlunch01.py
+++ Module 1 - Omgaan met/deel_1/feestlunch01.py
@@ -1,10 +1,18 @@
+# kosten van de feestlunch
 CROISSANTJES = 0.39
-#print(type(CROISSANTJES)) laat zien wat voor type het is
 STOKBRODEN = 2.78
-#totaal = 17 * croissantjes + 2 * stokbroden
 KORTINGSBONNEN = 0.50
-totaal = 17 * CROISSANTJES + 2 * STOKBRODEN - 3 * KORTINGSBONNEN
+#aantal
+AANTAL_CROISSANTJES = 17
+AANTAL_STOKBRODEN = 2
+AANTAL_KORTINGSBONNEN = 3
+#prijs
+prijs_croissantjes = AANTAL_CROISSANTJES * CROISSANTJES
+prijs_stokbroden = AANTAL_STOKBRODEN * STOKBRODEN
+prijs_kortingsbonnen = AANTAL_KORTINGSBONNEN * KORTINGSBONNEN
+#totaal
+totaal = prijs_croissantjes + prijs_stokbroden - prijs_kortingsbonnen
 if totaal > 0:
-    print(f"De totale kosten zijn: {totaal} euro")
+    print(f"De feestlunch kost je bij de bakker: {totaal} euro")
 elif totaal == 18.88:
-    print(f'De feestlunch kost je bij de bakker 18.88 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!')
+    print(f'De feestlunch kost je bij de bakker 18.88 euro voor de {AANTAL_CROISSANTJES} croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!')
--- Module 1 - Omgaan met/deel_1/speelhal02.py
+++ Module 1 - Omgaan met/deel_1/speelhal02.py
@@ -3,15 +3,16 @@ PERSOONS_PRIJS = 7.45
 PRIJS_PER_VIJF_MINUUT = 0.37
 AANTAL_MINUTEN = 45
 AANTAL_RONDJES = 9
-
+#totaal_kosten_gameseat = ((TIJD_GAMESEAT_MINUTEN / 5) * PRIJS_PER_VIJF_MINUUT) * AANTAL_MENSEN
 totaal_kosten_gameseat = ((AANTAL_MINUTEN / 5) * PRIJS_PER_VIJF_MINUUT) * AANTAL_MENSEN
-
 persoons_kosten = PERSOONS_PRIJS * AANTAL_MENSEN
 # 45 minuten VR kost 9 rondjes van 5 minuten
+#gameseat_kosten = PRIJS_PER_VIJF_MINUUT * AANTAL_RONDJES
 
 totaal = persoons_kosten + totaal_kosten_gameseat
-
+#print(f"De totale kosten zijn: {totaal} euro")
 if totaal > 0:
+    #print(f"De totale kosten zijn: {totaal} euro")
     print(f'Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar {totaal} euro')
 elif totaal == 44.44:
     print(f'Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 44.44 euro')
