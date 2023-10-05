bff6c4c987a8d5146906806a34bbcbd086f87c2c
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-10-05 11:43:37 +0200

    verder gewerkt

--- Module 1 - Omgaan met/deel_2/oliebol02.py
+++ Module 1 - Omgaan met/deel_2/oliebol02.py
@@ -5,11 +5,16 @@ OLIEBOL_PRIJS = 99
 PRIJS_10_OLIEBOLLEN = 750
 APPEPFLAP_PRIJS = 149
 
-AANTAL_OLIEBOLLEN = 11
+AANTAL_OLIEBOLLEN = 725
 AANTAL_APPLEFLAPPEN = 0
 
+aantal_zaken = AANTAL_OLIEBOLLEN // 10
+aantal_losse = AANTAL_OLIEBOLLEN % 10
+oliebollen_totaal = aantal_zaken * PRIJS_10_OLIEBOLLEN + aantal_losse * OLIEBOL_PRIJS
+
 # BEREKENINGEN
-prijs_totaal = AANTAL_OLIEBOLLEN * OLIEBOL_PRIJS + AANTAL_APPLEFLAPPEN * APPEPFLAP_PRIJS
+
+prijs_totaal = oliebollen_totaal + AANTAL_APPLEFLAPPEN * APPEPFLAP_PRIJS
 totaal_eur = prijs_totaal / 100   
 
 print(f"De {AANTAL_OLIEBOLLEN} oliebollen en {AANTAL_APPLEFLAPPEN} appelflappen kosten samen: {totaal_eur}")
