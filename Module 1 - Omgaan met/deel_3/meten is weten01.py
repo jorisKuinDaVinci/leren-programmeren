8868ad3ca8b6310edcb36dac48f64729e66681e5
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-10-09 15:56:02 +0200

    min en max

--- Module 1 - Omgaan met/deel_3/meten is weten01.py
+++ Module 1 - Omgaan met/deel_3/meten is weten01.py
@@ -1,11 +1,15 @@
 getal_a = int(input("Geef een getal: "))
 getal_b = int(input("Geef een getal: "))
+max = getal_a
+min = getal_b
 if getal_a > getal_b:
-    max = getal_a
+    #max = getal_a
     print("a is het grootste getal: " + str(max))
 elif getal_a < getal_b:
-    min = getal_b
+    #min = getal_b
     print("a is het kleinste getal: " + str(min))    
 else:
     print("a en b zijn even groot")
-        
+
+print("het minimum is: " + str(min))    
+print("het maximum is: " + str(max))   
