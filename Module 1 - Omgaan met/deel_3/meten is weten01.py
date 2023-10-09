367c9367104821e24d8a852cd650bbeb26aa9305
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-10-09 15:51:15 +0200

    elif-statement

--- Module 1 - Omgaan met/deel_3/meten is weten01.py
+++ Module 1 - Omgaan met/deel_3/meten is weten01.py
@@ -3,3 +3,7 @@ getal_b = int(input("Geef een getal: "))
 if getal_a > getal_b:
     max = getal_a
     print("a is het grootste getal: " + str(max))
+elif getal_a < getal_b:
+    min = getal_b
+    print("a is het kleinste getal: " + str(min))    
+    
