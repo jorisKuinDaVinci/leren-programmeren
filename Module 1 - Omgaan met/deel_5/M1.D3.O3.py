56228edbfdf3a5b7598195c23825f5e083b8140b
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-10-30 14:18:01 +0100

    verder gewerkt

--- Module 1 - Omgaan met/deel_5/M1.D3.O3.py
+++ Module 1 - Omgaan met/deel_5/M1.D3.O3.py
@@ -6,3 +6,25 @@ C = randint(-1000, 1000)
 D = randint(-1000, 1000)
 
 # bouw hier je nested if
+if A < B:
+    if A < C:
+        if A < D:
+            if B > C:
+                if B > D:
+                    if C < D:
+                        print("getal A", A, "is wat het is, getal C", C, "is groter, getal D", D, "is nog groter, maar getal B", B, "is het grootst!")
+                    else:
+                        if D < C:
+                            if B > D:
+                                if D > A:
+                                    print("getal A", A, "is wat het is, getal D", D, "is groter, getal B", B, "is nog groter, maar getal C", C, "is het grootst!")
+                                else:
+                                    print(A)
+                            else:
+                                print(D)
+                        else:
+                            if C > D:
+                                print(C)
+                            else:
+                                print(D)
+
