e62fe67dea653f5a50bdfbf85a860ed17e3a47c2
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-11-10 11:28:24 +0100

    verder gewerkt

--- Module 1 - Omgaan met/deel_5_04/meten is weten.py
+++ Module 1 - Omgaan met/deel_5_04/meten is weten.py
@@ -9,20 +9,9 @@ def grootste_getal(nr1: int, nr2: int) -> str:
         return 'Beide getallen zijn even groot'
 nr1 = 3
 nr2 = 11
-nr3 = 37
-nr4 = 79
 
 result1 = grootste_getal(nr1, nr2)
 result2 = f'Maximum: {nr2} en minimum: {nr1}'
 test('grootste_getal', result1, result2)
 
-result3 = grootste_getal(nr3, nr4)
-result4 = f'Maximum: {nr4} en minimum: {nr3}'
-test('grootste_getal', result3, result4)
-
-result5 = grootste_getal(nr1, nr1)
-
-
-report()
-
-   
+report() 
