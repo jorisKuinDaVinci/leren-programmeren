deda416c5b9099b45fff197d5540b1fd71fb0e30
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-11-16 11:32:10 +0100

    verder gewerkt

--- Module 1 - Omgaan met/deel_6/Data_opzoeken.py
+++ Module 1 - Omgaan met/deel_6/Data_opzoeken.py
@@ -5,10 +5,3 @@ def get_value(data: str, separator: str, position: int) -> str:
   else:
     value = None
   return value
-
-if __name__ == '__main__':
-  toets_data = 'Sofie:8,Emma:7,Ahmed:9,Daan:6,Lisa:8,Fatima:7,Ruben:9,Ayoub:6,Bram:6,Maria:7'
-  separator = ','
-  position= 8 # position of Bram, first position starts with 0
-  value = get_value(toets_data, separator, position)
-  print(value) # prints: Bram:6
