6ec9d32e9fc80333e90cefb46dd55d57f1bc1c93
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-11-16 11:38:41 +0100

    verder gewerkt

--- Module 1 - Omgaan met/deel_6/Data_opzoeken02.py
+++ Module 1 - Omgaan met/deel_6/Data_opzoeken02.py
@@ -2,5 +2,5 @@ from Data_opzoeken import get_value
 toets_data = 'Sofie:8,Emma:7,Ahmed:9,Daan:6,Lisa:8,Fatima:7,Ruben:9,Ayoub:6,Bram:6,Maria:7'
 separator = ','
 position= 8 # positie van Bram, eerste positie start met 0
-value = get_value(toets_data, separator, position)
-print(value) # prints: Bram:6
+result = get_value(toets_data, separator, position)
+print(result) # prints: Bram:6
