f28d5bb1621f4d1038deb7cc7434ab5fa7155b31
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-11-16 11:47:26 +0100

    verder gewerkt

--- Module 1 - Omgaan met/deel_6/Data_opzoeken.py
+++ Module 1 - Omgaan met/deel_6/Data_opzoeken.py
@@ -6,3 +6,9 @@ def get_value(data: str, separator: str, position: int) -> str:
     else:
       value = None
     return value
+
+toets_data = 'Sofie:8,Emma:7,Ahmed:9,Daan:6,Lisa:8,Fatima:7,Ruben:9,Ayoub:6,Bram:6,Maria:7'
+separator = ','
+position= 8 # positie van Bram, eerste positie start met 0
+result = get_value(toets_data, separator, position)
+print(result) # prints: Bram:6
--- Module 1 - Omgaan met/deel_6/Data_opzoeken02.py
+++ Module 1 - Omgaan met/deel_6/Data_opzoeken02.py
@@ -1,4 +1,12 @@
-from Data_opzoeken import get_value
+def get_value(data: str, separator: str, position: int) -> str:
+    splitted_strings = data.split(separator) 
+  
+    if 0 <= position< len(splitted_strings):
+      value = splitted_strings[position] 
+    else:
+      value = None
+    return value
+
 toets_data = 'Sofie:8,Emma:7,Ahmed:9,Daan:6,Lisa:8,Fatima:7,Ruben:9,Ayoub:6,Bram:6,Maria:7'
 separator = ','
 position= 8 # positie van Bram, eerste positie start met 0
