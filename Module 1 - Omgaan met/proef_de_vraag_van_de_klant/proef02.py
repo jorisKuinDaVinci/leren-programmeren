3d01511ed4713e53007e57725b8e06ff87b049eb
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-12-04 13:18:20 +0100

    verder gewerkt

--- Module 1 - Omgaan met/proef_de_vraag_van_de_klant/proef02.py
+++ Module 1 - Omgaan met/proef_de_vraag_van_de_klant/proef02.py
@@ -1,19 +1,3 @@
-#Deze opdracht moet aan de volgende functionele eisen voldoen:
-
-#! GEEN QUIZ MET ALLEEN (LINEAIR) VRAGEN !
-
-#het spel heeft tenminste 10 acties waar een beslissing moet worden genomen;
-#het spel kent een "win" einde;
-#het spel kent minmaal 2 "faal" eindes;
-#het spel kent meerdere routes en meerdere eindpunten, je kunt het spel dus meerdere keren spelen met verschillende uitkomsten;
-#Als inspiratie kun je denken aan de oude text-based adventure games als 'Zork'.
-
-#Voorbeeld van een spel:
-# je bent een persoon die in een route loopt
-# je komt een splitsing tegen
-# je kunt kiezen uit 2 routes
-# wat je kiest bepaalt hoe het spel verder gaat en of je wel of niet overleeft
-
 naam = input('wat is je naam?')
 print('hallo ' + naam + ' je loopt in een bos. je bent verdwaald')
 keuze_1 = input('je staat voor een splitsing, welke kant kiest je? (links of rechts)')
