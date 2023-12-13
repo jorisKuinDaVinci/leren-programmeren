c63af98e4ae66acafac7a8ee457d0b9abac568ad
jorisKuinDaVinci <99074486@mydavinci.nl>
2023-12-13 13:29:12 +0100

    geen extra ifs

--- Module 1 - Omgaan met/proef_de_vraag_van_de_klant_spel/proef02.py
+++ Module 1 - Omgaan met/proef_de_vraag_van_de_klant_spel/proef02.py
@@ -6,8 +6,7 @@ if naam == naam_1:
 else:
     print('hallo ' + naam + ' je loopt door een bos. je bent verdwaald')
 keuze_1 = input('je staat voor een splitsing, welke kant kiest je? (links of rechts)')
-if keuze_1 not in ['links', 'rechts']:
-    while True: 
+while keuze_1 not in ['links', 'rechts']: 
             print("Dat is niet een keuze!(kies links of rechts!)")
             keuze_1 = input('je staat voor een splitsing, welke kant kiest je? (links of rechts)')
 if keuze_1 == 'links':
@@ -19,15 +18,13 @@ elif keuze_1 == 'rechts':
     print('je loopt verder en je komt aan bij een huis')
     goed_antwoord_1 = 'ja'
     keuze_2 = input('je kunt kiezen om aan te bellen of om weg te lopen, wat doe je? (aanbellen of weglopen)')
-    if keuze_2 not in ['aanbellen', 'weglopen']:
-        while True:
+    while keuze_2 not in ['aanbellen', 'weglopen']:
                 print("Dat is niet een keuze!(kies aanbellen of weglopen!)")
                 keuze_2 = input('je kunt kiezen om aan te bellen of om weg te lopen, wat doe je? (aanbellen of weglopen)')
     if keuze_2 == 'aanbellen':
         goed_antwoord_2 = 'nee'
         keuze_2_5 = input('er doet niemand open je ziet een apparraat aan het huis hangen, je moet een code invoeren. a (code invoeren) of weglopen')
-        if keuze_2_5 not in ['a', 'weglopen']:
-            while True: 
+        while keuze_2_5 not in ['a', 'weglopen']: 
                     print("Dat is niet een keuze! kies a (code invoeren) of weglopen!")
                     keuze_2_5 = input('er doet niemand open je ziet een apparraat aan het huis hangen, je moet een code invoeren. a (code invoeren) of weglopen')
         if keuze_2_5 == 'a':
@@ -40,15 +37,13 @@ elif keuze_1 == 'rechts':
             elif keuze_2_6 == '104':
                 print('je hebt de code goed ingevoerd en de deur gaat open.')
                 keuze_3 = input('er staat een oude vrouw in het huis, ze vraagt of je binnen wilt komen, wat doe je? (b (naar binnen gaan) of weglopen)')
-                if keuze_3 not in ['b', 'weglopen']:
-                    while True:
+                while keuze_3 not in ['b', 'weglopen']:
                             print("Dat is niet een keuze! kies b (naar binnen gaan) of weglopen!")
                             keuze_3 = input('er staat een oude vrouw in het huis, ze vraagt of je binnen wilt komen, wat doe je? (b (naar binnen gaan) of weglopen)')
                 if keuze_3 == 'b':
                     print('je komt binnen en de vrouw vraagt of je wat wilt drinken.')
                     keuze_4 = input('wil je drinken? (ja of nee)')
-                    if keuze_4 not in ['ja', 'nee']:
-                        while True:
+                    while keuze_4 not in ['ja', 'nee']:
                                 print("Dat is niet een keuze!(kies ja of nee!)")
                                 keuze_4 = input('wil je drinken? (ja of nee)')
                     if keuze_4 == 'ja':
@@ -71,8 +66,7 @@ elif keuze_1 == 'rechts':
         print('je loopt weg en komt aan bij een rivier')
         goed_antwoord_2 = 'ja'
         keuze_5 = input('er is een brug. je kunt kiezen om te zwemmen of om over de brug te lopen, wat doe je? zwemmen of brug (over de brug lopen)')
-        if keuze_5 not in ['zwemmen', 'brug']:
-            while True:
+        while keuze_5 not in ['zwemmen', 'brug']:
                     print("Dat is niet een keuze! kies zwemmen of brug (over de brug lopen)!")
                     keuze_5 = input('er is een brug. je kunt kiezen om te zwemmen of om over de brug te lopen, wat doe je? zwemmen of brug (over de brug lopen)')
         if keuze_5 == 'brug':
@@ -84,9 +78,8 @@ elif keuze_1 == 'rechts':
             print('je gaat zwemmen en komt veilig aan de overkant.')
             print('je loopt verder en komt aan bij een weg')
             goed_antwoord_3 = 'ja'
-            keuze_6 = input('je kunt kiezen om te oversteken of verder door het bos te lopen wat doe je? oversteken of bos (door het bos lopen)') 
-            if keuze_6 not in ['oversteken', 'bos']:
-                while True:
+            keuze_6 = input('je kunt kiezen om te oversteken of verder door het bos te lopen wat doe je? oversteken of bos (door het bos lopen)')
+            while keuze_6 not in ['oversteken', 'bos']:
                         print("Dat is niet een keuze! kies oversteken of bos (door het bos lopen)!")
                         keuze_6 = input('je kunt kiezen om te oversteken of verder door het bos te lopen wat doe je? oversteken of bos (door het bos lopen)')
             if keuze_6 == 'oversteken':
@@ -98,8 +91,7 @@ elif keuze_1 == 'rechts':
                 print('je loopt door het bos en komt aan bij een zebrapad')
                 goed_antwoord_4 = 'ja'
                 keuze_7 = input('je kunt kiezen om over te steken of om te wachten wat doe je? (oversteken of wachten)')
-                if keuze_7 not in ['oversteken', 'wachten']:
-                    while True:
+                while keuze_7 not in ['oversteken', 'wachten']:
                             print("Dat is niet een keuze!(kies oversteken of wachten!)")
                             keuze_7 = input('je kunt kiezen om over te steken of om te wachten wat doe je? (oversteken of wachten)')
                 if keuze_7 == 'wachten':
@@ -112,8 +104,7 @@ elif keuze_1 == 'rechts':
                     goed_antwoord_5 = 'ja'
                     print('je loopt verder en komt aan bij een huis met een bordje "te koop"')
                     keuze_8 = input('je kunt kiezen om het huis te kopen of om verder te lopen wat doe je? huis (huis kopen) of verder (verder lopen)')
-                    if keuze_8 not in ['huis', 'verder']:
-                        while True:
+                    while keuze_8 not in ['huis', 'verder']:
                                 print("Dat is niet een keuze! kies huis (huis kopen) of verder (verder lopen!)")
                                 keuze_8 = input('je kunt kiezen om het huis te kopen of om verder te lopen wat doe je? huis (huis kopen) of verder (verder lopen)')
                     if keuze_8 == 'huis':
@@ -125,16 +116,14 @@ elif keuze_1 == 'rechts':
                         goed_antwoord_6 = 'ja'
                         print('je loopt verder en komt bij een kruispunt')
                         keuze_9 = input('je kunt kiezen om links of rechts te gaan wat doe je? (links of rechts)')
-                        if keuze_9 not in ['links', 'rechts']:
-                            while True:
+                        while keuze_9 not in ['links', 'rechts']:
                                     print("Dat is niet een keuze! kies links of rechts!")
                                     keuze_9 = input('je kunt kiezen om links of rechts te gaan wat doe je? (links of rechts)')
                         if keuze_9 == 'links':
                             goed_antwoord_7 = 'nee'
                             print('je gaat links en ziet iets bekends')
                             keuze_10 = input('je kunt kiezen om er naar toe te gaan of om verder te lopen wat doe je? a (er naar toe gaan) of verder (verder lopen)')
-                            if keuze_10 not in ['a', 'verder']:
-                                while True:
+                            while keuze_10 not in ['a', 'verder']:
                                         print("Dat is niet een keuze! kies a (er naar toe gaan) of verder (verder lopen)")
                                         keuze_10 = input('je kunt kiezen om er naar toe te gaan of om verder te lopen wat doe je? a (er naar toe gaan) of verder (verder lopen)')
                             if keuze_10 == 'a':
@@ -145,15 +134,13 @@ elif keuze_1 == 'rechts':
                             elif keuze_10 == 'verder':
                                 print('je loopt verder en komt aan bij een huis')
                                 keuze_11 = input('je kunt kiezen om aan te bellen of om weg te lopen wat doe je? (aanbellen of weglopen)')
-                                if keuze_11 not in ['aanbellen', 'weglopen']:
-                                    while True:
+                                while keuze_11 not in ['aanbellen', 'weglopen']:
                                             print("Dat is niet een keuze! kies aanbellen of weglopen!")
                                             keuze_11 = input('je kunt kiezen om aan te bellen of om weg te lopen wat doe je? (aanbellen of weglopen)')
                                 if keuze_11 == 'aanbellen':
                                     print('je belt aan en er doet een oude vrouw open')
                                     keuze_12 = input('ze vraagt of je binnen wilt komen wat doe je? a (naar binnen gaan) of weglopen')
-                                    if keuze_12 not in ['a', 'weglopen']:
-                                        while True:
+                                    while keuze_12 not in ['a', 'weglopen']:
                                                 print("Dat is niet een keuze! kies a (naar binnen gaan) of weglopen!")
                                                 keuze_12 = input('ze vraagt of je binnen wilt komen wat doe je? a (naar binnen gaan) of weglopen')
                                     if keuze_12 == 'a':
