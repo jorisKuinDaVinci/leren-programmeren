67896c64151b44ac7205dac761105d1d2d0cd523
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-01-22 15:41:42 +0100

    deck van speelkaarten afgerond

--- Module 2 - Werken met/Deel 2 - collections/Deck_van_speelkaarten/Deck_van_speelkaarten.py
+++ Module 2 - Werken met/Deel 2 - collections/Deck_van_speelkaarten/Deck_van_speelkaarten.py
@@ -2,14 +2,23 @@
 import random
 
 kleuren = ("harten", "klaveren", "schoppen", "ruiten")
+
 kaarten = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "boer", "vrouw", "heer", "aas")
+
 jokers = ("joker", "joker")
+
 deck = []
+
 for i in range(len(kleuren)):
     for j in range(len(kaarten)):
         deck.append(kaarten[j] + " " + kleuren[i])
+
 for i in range(len(jokers)):
     deck.append(jokers[i])
+
 random.shuffle(deck)
-print(deck[0:7])
+
+for i in range(len(deck)):
+    print("Kaart " + str(i + 1) + ": " + deck[i])
+
 print("deck (47 kaarten): " + str(deck))
