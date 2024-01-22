a4440e23dd971a58eab26b0d926cef13a82a8eeb
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-01-22 15:33:09 +0100

    garage opdracht bijgewerkt en deck van speelkaarten begonnen

--- Module 1 - Omgaan met/extra_lessen/extratest02/garage.py
+++ Module 1 - Omgaan met/extra_lessen/extratest02/garage.py
@@ -5,3 +5,11 @@ from extratest0202 import *
 print(len(autos))
 
 # hoe print ik alleen de auto met de kleinste moterinhoud! in 1 regel.
+
+def get_motorinhoud(a):
+    return a['motorinhoud']
+auto_kleinste_motor = min(autos, key=get_motorinhoud)
+print(auto_kleinste_motor)
+
+auto_kleinste_motor2 = min(autos, key=lambda a: a['motorinhoud'])
+print(auto_kleinste_motor2)
---
+++ Module 2 - Werken met/Deel 2 - collections/Deck_van_speelkaarten/Deck_van_speelkaarten.py
@@ -0,0 +1,15 @@
+# Deel 2 - collections - Deck van speelkaarten
+import random
+
+kleuren = ("harten", "klaveren", "schoppen", "ruiten")
+kaarten = ("2", "3", "4", "5", "6", "7", "8", "9", "10", "boer", "vrouw", "heer", "aas")
+jokers = ("joker", "joker")
+deck = []
+for i in range(len(kleuren)):
+    for j in range(len(kaarten)):
+        deck.append(kaarten[j] + " " + kleuren[i])
+for i in range(len(jokers)):
+    deck.append(jokers[i])
+random.shuffle(deck)
+print(deck[0:7])
+print("deck (47 kaarten): " + str(deck))
