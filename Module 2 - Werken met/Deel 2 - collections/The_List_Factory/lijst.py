b637dc68fbfc5c8c99a6dc40886f20187ccd2f33
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-02-07 21:24:23 +0100

    commit 2

--- Module 2 - Werken met/Deel 2 - collections/The_List_Factory/lijst.py
+++ Module 2 - Werken met/Deel 2 - collections/The_List_Factory/lijst.py
@@ -1,3 +1,16 @@
 from string import ascii_lowercase
 print(ascii_lowercase)
 print(ascii_lowercase[1:5:2])
+
+input_lijst = input("Hoeveel lijstjes wil je? ")
+input_lijst = int(input_lijst)
+lijst = []
+for i in range(input_lijst):
+    input_letters = input("Hoeveel letters wil je in lijstje " + str(i + 1) + "? ")
+    input_letters = int(input_letters)
+    lijst.append([])
+    for j in range(input_letters):
+        input_letter = input("Geef letter " + str(j + 1) + " van lijstje " + str(i + 1) + ": ")
+        lijst[i].append(input_letter)
+print(lijst)
+print(ascii_lowercase(lijst[0]))
