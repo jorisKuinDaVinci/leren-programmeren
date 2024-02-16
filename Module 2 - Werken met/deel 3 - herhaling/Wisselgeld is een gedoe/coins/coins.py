0e1317c225069dea921f53daf8682fa89a074df8
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-02-16 14:10:27 +0100

    uitleg coins.py

--- Module 2 - Werken met/deel 3 - herhaling/Wisselgeld is een gedoe/coins/coins.py
+++ Module 2 - Werken met/deel 3 - herhaling/Wisselgeld is een gedoe/coins/coins.py
@@ -1,26 +1,21 @@
-# name of student: Joris
-# number of student:
-# purpose of program: 
-# structure of program: 
+coinValues = [50,20,10,5,2,1] # de waardes van de munten
 
-coinValues = [50,20,10,5,2,1] #
+toPay = int(float(input('Amount to pay: '))* 100) # input van wat je moet betalen
+paid = int(float(input('Paid amount: ')) * 100) # input van wat je hebt betaald 
+change = paid - toPay # wat je hebt betaald - wat je moet betalen
 
-toPay = int(float(input('Amount to pay: '))* 100) #
-paid = int(float(input('Paid amount: ')) * 100) #
-change = paid - toPay #
+while change > 0 and len(coinValues) > 0: # zolang er nog wisselgeld is en er nog munten zijn
 
-while change > 0 and len(coinValues) > 0: #
+  coinValue = coinValues.pop(0) # coinValue is de eerste waarde van coinValues
+  nrCoins = change // coinValue # wat je terug krijgt / de waarde van de munt
 
-  coinValue = coinValues.pop(0) #
-  nrCoins = change // coinValue #
-
-  if nrCoins > 0: #
-    print('return maximal ', nrCoins, ' coins of ', coinValue, ' cents!' ) #
-    nrCoinsReturned = int(input('How many coins of ' + str(coinValue) +  ' cents did you return? ')) #
-    change -= nrCoinsReturned * coinValue # 
+  if nrCoins > 0: # als je munten terug krijgt
+    print('return maximal ', nrCoins, ' coins of ', coinValue, ' cents!' ) # hoeveel munten je terug krijgt
+    nrCoinsReturned = int(input('How many coins of ' + str(coinValue) +  ' cents did you return? ')) # hoeveel munten je teruggeeft
+    change -= nrCoinsReturned * coinValue # wat je terug krijgt - wat je hebt teruggegeven * de waarde van de munt
 
 
-if change > 0: #
-  print('Change not returned: ', str(change) + ' cents') #
+if change > 0: # als je nog steeds wisselgeld hebt
+  print('Change not returned: ', str(change) + ' cents') # wat je terug krijgt
 else:
   print('done')
