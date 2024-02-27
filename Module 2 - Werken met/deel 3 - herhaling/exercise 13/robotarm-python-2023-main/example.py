8f50d456e71937b872093c8e12b09dda1a4a073d
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-02-27 13:50:39 +0100

    robotarm opdracht 12 en 13 verder gewerkt

--- Module 2 - Werken met/deel 3 - herhaling/exercise 13/robotarm-python-2023-main/example.py
+++ Module 2 - Werken met/deel 3 - herhaling/exercise 13/robotarm-python-2023-main/example.py
@@ -5,7 +5,20 @@ robotArm.randomLevel(1,7)
 
 # Jouw python instructies zet je vanaf hier:
 robotArm.speed = 3
-robotArm._animateHazard
+#Verdeel alle blokken over de lege plaatsen, zodra er geen blokken meer zijn moet de arm stoppen.
+for i in range(9):
+    robotArm.grab()
+    color = robotArm.scan()
+    if color != "":
+        for i in range(9):
+            robotArm.moveRight()
+        robotArm.drop()
+        for i in range(9):
+            robotArm.moveLeft()
+    else:
+        robotArm.drop()
+    robotArm.moveRight()
+
 
 
 # Na jouw code wachten tot het sluiten van de window:
