f1ffa7d12c63d25a7cb0bbd3187e0949054d3405
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-02-29 11:58:07 +0100

    robotarm 13 bijgewerkt

--- Module 2 - Werken met/deel 3 - herhaling/exercise 13/robotarm-python-2023-main/example.py
+++ Module 2 - Werken met/deel 3 - herhaling/exercise 13/robotarm-python-2023-main/example.py
@@ -4,18 +4,8 @@ robotArm = RobotArm()
 robotArm.randomLevel(1,7)
 
 # Jouw python instructies zet je vanaf hier:
-robotArm.speed = 3
-check_for_blocks = True
-while check_for_blocks:
-    robotArm.grab()
-    if robotArm.scan() == "":
-        check_for_blocks = False
-    else:
-        for i in range(9):
-            robotArm.moveRight()
-        robotArm.drop()
-        for i in range(9):
-            robotArm.moveLeft()
+
+
 
 
 
