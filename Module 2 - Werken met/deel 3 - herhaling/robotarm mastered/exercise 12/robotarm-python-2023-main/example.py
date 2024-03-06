d71abd4b4a3e78a04c9b82f85ef4278fba4c3874
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-03-06 11:43:37 +0100

    robotarm 13 afgerond

--- Module 2 - Werken met/deel 3 - herhaling/robotarm mastered/exercise 12/robotarm-python-2023-main/example.py
+++ Module 2 - Werken met/deel 3 - herhaling/robotarm mastered/exercise 12/robotarm-python-2023-main/example.py
@@ -3,22 +3,8 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 12')
 
 # Jouw python instructies zet je vanaf hier:
-#color = robotArm.scan()
 robotArm.speed = 2
-robotArm.operate()
 
-#for i in range(9):
-    #robotArm.grab()
-    #color = robotArm.scan()
-    #if color == "red":
-        #for i in range(9):
-            #robotArm.moveRight()
-        #robotArm.drop()
-        #for i in range(9):
-            #robotArm.moveLeft()
-    #else:
-        #robotArm.drop()
-    #robotArm.moveRight()
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
--- Module 2 - Werken met/deel 3 - herhaling/robotarm mastered/exercise 13/robotarm-python-2023-main/example.py
+++ Module 2 - Werken met/deel 3 - herhaling/robotarm mastered/exercise 13/robotarm-python-2023-main/example.py
@@ -5,19 +5,17 @@ robotArm.randomLevel(1,7)
 
 # Jouw python instructies zet je vanaf hier:
 robotArm.speed = 3
-#Verdeel alle blokken over de lege plaatsen, zodra er geen blokken meer zijn moet de arm stoppen.
-for i in range(9):
+for stapel in range(9):
     robotArm.grab()
     color = robotArm.scan()
     if color != "":
-        for i in range(9):
+        for i in range(9-stapel):
             robotArm.moveRight()
         robotArm.drop()
-        for i in range(9):
+        for i in range(9-stapel):
             robotArm.moveLeft()
     else:
-        robotArm.drop()
-    robotArm.moveRight()
+        break
 
 
 
