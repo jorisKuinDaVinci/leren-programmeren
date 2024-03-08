bb18e0db70efc1e78b7e706eb3b537c8f6b5d60c
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-03-08 13:45:53 +0100

    robotarm 13 bijgewerkt en afgerond

--- Module 2 - Werken met/deel 3 - herhaling/robotarm mastered/exercise 13/robotarm-python-2023-main/example.py
+++ Module 2 - Werken met/deel 3 - herhaling/robotarm mastered/exercise 13/robotarm-python-2023-main/example.py
@@ -2,7 +2,9 @@ from RobotArm import RobotArm
 # Let op: hier start het anders voor een random level:
 robotArm = RobotArm()
 robotArm.randomLevel(1,7)
+
 robotArm.speed = 3
+
 for stapel in range(9):
     robotArm.grab()
     color = robotArm.scan()
