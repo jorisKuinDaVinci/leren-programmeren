576af4e6644baa9b23fec8361988d33547034ca9
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-04-08 11:39:11 +0200

    fruitmand opdracht 6 en 9 afgerond

--- Module 2 - Werken met/deel 5 - een dataset/fruitmand/opdracht09.py
+++ Module 2 - Werken met/deel 5 - een dataset/fruitmand/opdracht09.py
@@ -1,8 +1,8 @@
 import fruitmand
 #Verwijder (met code) de druif uit de fruitmand en print alleen alle verschillende kleuren in de fruitmand, in maximaal 8 regels code (minus lege regels en de import).
-fruitmand.fruitmand.pop(4)
-colors = []
-for fruit in fruitmand.fruitmand:
-    if fruit['color'] not in colors:
-        colors.append(fruit['color'])
-        print(fruit['color'])
+# zorg er voor dat het ook werkt als het op een andere regel staat.
+fruitmand = fruitmand.fruitmand
+fruitmand.remove("druif")
+kleuren = set()
+for fruit in fruitmand:
+    kleuren.add(fruit["kleur"])
--- Module 2 - Werken met/deel 5 - een dataset/fruitmand2/opdracht06.py
+++ Module 2 - Werken met/deel 5 - een dataset/fruitmand2/opdracht06.py
@@ -1,3 +1,7 @@
 import fruitmand
 #Print vanuit de fruitmand alleen het gewicht van de appel, gebruik maximaal 4 regels code (minus lege regels en de import).
-print(fruitmand.fruitmand[5]['weight'])
+# zorg er voor dat het ook werkt als het op een andere regel staat.
+fruitmand = fruitmand.fruitmand
+for fruit in fruitmand:
+    if fruit["naam"] == "appel":
+        print(fruit["gewicht"])
