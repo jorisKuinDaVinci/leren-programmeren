f1ab7e5b3c5b404b243963453bc0cc92b19ea4d5
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-04-08 12:53:23 +0200

    fruitmand bijgewerkt

--- Module 2 - Werken met/deel 5 - een dataset/fruitmand/opdracht04.py
+++ Module 2 - Werken met/deel 5 - een dataset/fruitmand/opdracht04.py
@@ -1,6 +1,11 @@
 import fruitmand
+from random import choice
+fruitmand_kopie = fruitmand.fruitmand.copy()
 #vraag om een aantal
-aantal = input("Hoeveel fruit wil je zien? ")
-#Print het opgegeven aantal keer een random fruitnaam uit de fruitmand. (dus bijv. niet 10 x appel)
-for i in range(int(aantal)):
-    print(fruitmand.fruitmand[i]['name'])
+aantal = int(input("Hoeveel fruit wil je zien? "))
+#Print het opgegeven aantal keer een random fruitnaam uit de fruitmand. (dus bijv. niet 10 x appel) echt random!
+for i in range(aantal):
+    fruit = choice(fruitmand_kopie)
+    print(fruit['name'])
+#for i in range(int(aantal)):
+    #print(fruitmand.fruitmand[i]['name'])
