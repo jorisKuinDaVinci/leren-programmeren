361f2dd4409ae0fb4c8d4a921b45a10086459f85
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-06-03 11:28:19 +0200

    opdracht bijgewerkt

--- Module 3 - Inzetten van/Deel 1 - doelgerichte functions/tafel.py
+++ Module 3 - Inzetten van/Deel 1 - doelgerichte functions/tafel.py
@@ -1,7 +1,24 @@
 tafel = 1
-for _ in range(1, 101):
-    for tafel in range(1, 101):
-        print("")
-        print(f"tafel van {tafel}: ")
-        for cijfer in range(1, 11):
-            print(f"{cijfer} x {tafel} = {cijfer * tafel}")      
+for tafel in range(1, 3):
+    print("")
+    print(f"tafel van {tafel}: ")
+    for cijfer in range(1, 11):
+        print(f"{cijfer} x {tafel} = {cijfer * tafel}")      
+
+# print onder elkaar alle cijfers van 99 t/m 215.
+# print onder elkaar 25 keer ik ben geweldig.
+# print onder elkaar ik vindt dit niet zo leuk.
+# print 25 keer onder elkaar ik ben geweldig met direct daaronder ik vindt dit saai worden.
+cijfer = 98
+for i in range(99, 216, 1):
+    cijfer += 1
+    print(cijfer)
+for i in range(25):
+    print("ik ben geweldig")
+for i in range(25):
+    print("ik vindt dit niet zo leuk")
+for geweldig in range(25):
+    print("ik ben geweldig") 
+    geweldig += 1
+    if geweldig == 25:
+        print("ik vindt dit saai worden")
