335e64059265a3923ddaef0b8ec1bb429a9dfeda
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-06-03 11:46:17 +0200

    opdracht bijgewerkt

--- Module 3 - Inzetten van/Deel 1 - doelgerichte functions/leeg.py
+++ Module 3 - Inzetten van/Deel 1 - doelgerichte functions/leeg.py
@@ -2,9 +2,6 @@ lijst = ["persoon2", "persoon1", "joris"]
 # lijst aan de functie geven
 # een hele lang zin. keer onder elkaar tot alle namen er staan.
 # begin met leert programeren
-def functie(lijst):
-    zin = ""
-    for naam in lijst:
-        zin += naam + " leert programeren\n"
-    return zin
-print(functie(lijst))
+for naam in lijst:
+    for i in range(2):
+        print(f"{naam} leert programeren\n")
