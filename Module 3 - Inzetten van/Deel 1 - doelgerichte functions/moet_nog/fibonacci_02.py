711e10b50a10864a4547a3a68cee3445c5241bf8
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-06-04 14:39:18 +0200

    fibonacci reeks def toegevoegt

--- Module 3 - Inzetten van/Deel 1 - doelgerichte functions/moet_nog/fibonacci_02.py
+++ Module 3 - Inzetten van/Deel 1 - doelgerichte functions/moet_nog/fibonacci_02.py
@@ -1,2 +1,13 @@
-# code
-# code
+def genereer_fibonacci_reeks(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    fib_reeks = [0, 1]
+    for i in range(2, n):
+        volgende_nummer = fib_reeks[-1] + fib_reeks[-2]
+        fib_reeks.append(volgende_nummer)
+    return fib_reeks
