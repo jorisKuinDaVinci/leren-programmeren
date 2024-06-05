7fbc87b64df11ef6bf8454769687044753f6c8d4
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-06-05 12:10:57 +0200

    fibonacci opdracht verbeterd

--- Module 3 - Inzetten van/Deel 1 - doelgerichte functions/moet_nog/fibonacci_03.py
+++ Module 3 - Inzetten van/Deel 1 - doelgerichte functions/moet_nog/fibonacci_03.py
@@ -7,7 +7,7 @@ def genereer_fibonacci_reeks(n):
         return [0, 1]
     
     fib_reeks = [0, 1]
-    for i in range(2, n):
+    for _ in range(2, n):
         volgende_nummer = fib_reeks[-1] + fib_reeks[-2]
         fib_reeks.append(volgende_nummer)
     return fib_reeks
@@ -26,12 +26,16 @@ fibonacci_reeks = genereer_fibonacci_reeks(aantal_getallen_fibonacci_reeks)
 # Bereken de Gulden Snede op basis van de laatste twee getallen
 if len(fibonacci_reeks) < 2:
     print("Niet genoeg getallen in de reeks om de Gulden Snede te berekenen.")
-    exit()
+else:
+    laatste_nummer = fibonacci_reeks[-1]
+    tweede_laatste_nummer = fibonacci_reeks[-2]
+    gulden_snede = genereer_gulden_snede(laatste_nummer, tweede_laatste_nummer)
 
-laatste_nummer = fibonacci_reeks[-1]
-tweede_laatste_nummer = fibonacci_reeks[-2]
-gulden_snede = genereer_gulden_snede(laatste_nummer, tweede_laatste_nummer)
-
-# Print de resultaten
-print(f"Eerste {aantal_getallen_fibonacci_reeks} getallen van de Fibonacci-reeks: {', '.join(map(str, fibonacci_reeks))}")
-print(f"De Gulden Snede berekend op basis van de laatste twee getallen ({laatste_nummer} en {tweede_laatste_nummer}) is: {gulden_snede}")
+    # Print de resultaten
+    print(f"Eerste {aantal_getallen_fibonacci_reeks} getallen van de Fibonacci-reeks: ", end="")
+    for i in range(len(fibonacci_reeks)):
+        if i == len(fibonacci_reeks) - 1:
+            print(fibonacci_reeks[i])
+        else:
+            print(fibonacci_reeks[i], end=", ")
+    print(f"De Gulden Snede berekend op basis van de laatste twee getallen ({laatste_nummer} en {tweede_laatste_nummer}) is: {gulden_snede}")
