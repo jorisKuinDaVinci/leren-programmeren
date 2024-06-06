d7feeb01b9234c54937ce26a5b05e8b21afe60de
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-06-06 09:49:36 +0200

    deuren lakken opdracht begin

---
+++ Module 1 - Omgaan met/deel_1/Deuren_lakken.py
@@ -0,0 +1,17 @@
+#Je wilt alle binnendeuren van je huis een ander kleurtje geven. Je gaat ze lakken, aan beide kanten!
+
+#Je hebt een kleur uitgekozen. Op de verfbus staat dat je met 750 ml verf 6 vierkante meter kunt verven.
+
+#Er zijn in jouw huis 13 binnendeuren.  Elke kant van de deur heeft een hoogte van 2 meter en een breedte van 80 cm.
+
+#Een bus verf kost € 23.45
+
+#Opdracht:
+#Maak een programma lakken.py dat het aantal te kopen bussen verf uitrekent en de kosten.
+#Bereken eerst de te verven oppervlakte in vierkante meters.
+#Halve bussen verf kan je niet kopen, dus moet je het aantal bussen naar boven naar hele bussen afronden met math.ceil(). Zoek uit hoe dat werkt.(w3schools.com)
+#Gebruik variabelen voor alle gegevens.
+#Je wilt de deuren eerst opschuren, dan hecht de verf beter. Op internet lees je dat je met een vel schuurpapier 1,5 vierkante meter kan opschuren. Één vel schuurpapier kost € 0.47
+
+#Breidt het programma lakken.py uit met een berekening voor het minimum aantal vellen schuurpapier en de kosten
+#Gebruik variabelen voor alle gegevens.
