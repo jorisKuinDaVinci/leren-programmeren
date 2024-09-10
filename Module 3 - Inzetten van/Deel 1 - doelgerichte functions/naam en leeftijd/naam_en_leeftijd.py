7fba9d6ca54a2d66d0865508566376071c92332a
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-09-10 11:08:32 +0200

    start naam en leeftijd

---
+++ Module 3 - Inzetten van/Deel 1 - doelgerichte functions/naam en leeftijd/naam_en_leeftijd.py
@@ -0,0 +1,12 @@
+#Je gaat een programma maken die een naam en leeftijd vraagt en op het scherm print. 
+
+#De volgende eisen worden gesteld aan de applicatie:
+
+#De gebruiker kan de namen en leeftijden zelf invullen via een input
+#Aan het einde van het programma word het resultaat netjes geprint zoals dit:
+
+#Rudi is 41 jaar
+
+#Voor het vragen van de naam en de leeftijd maak je een functie die dit voor je doet.
+#De output/return van deze functie is een dictionairy met de properties ‘name’ en ‘age’.
+#Buiten de functie doe je de print, door eerst de functie aan te roepen.
