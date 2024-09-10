fb654e168543afac8e6c9b95b3c9ba3ac05c9083
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-09-10 11:25:26 +0200

    eind namen en leeftijden verzamelen

--- Module 3 - Inzetten van/Deel 1 - doelgerichte functions/Namen en leeftijden verzamelen/Namen_en_leeftijden_verzamelen.py
+++ Module 3 - Inzetten van/Deel 1 - doelgerichte functions/Namen en leeftijden verzamelen/Namen_en_leeftijden_verzamelen.py
@@ -15,4 +15,13 @@ def vraag_naam_en_leeftijd():
 #print(f"{functie['naam']} is {functie['leeftijd']} jaar")
 
 def verzamel_naam_en_leeftijd():
-    
+    lijst = []
+    vraag = ""
+    while vraag != "stop":
+        lijst.append(vraag_naam_en_leeftijd())
+        vraag = input("Toets enter om door te gaan of stop om te printen: ")
+    return lijst
+
+functie = verzamel_naam_en_leeftijd()
+for i in functie:
+    print(f"{i['naam']} is {i['leeftijd']} jaar")
