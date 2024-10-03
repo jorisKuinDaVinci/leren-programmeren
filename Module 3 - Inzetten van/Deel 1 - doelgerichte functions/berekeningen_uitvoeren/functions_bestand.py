6189e252cb74cbd0e2542f4620de969cab2bf599
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-10-03 09:23:25 +0200

    verder

---
+++ Module 3 - Inzetten van/Deel 1 - doelgerichte functions/berekeningen_uitvoeren/functions_bestand.py
@@ -0,0 +1,13 @@
+def optellen(nummer1, nummer2):
+    return nummer1 + nummer2
+
+def min(nummer1, nummer2):
+    return nummer1 - nummer2
+
+def keer(nummer1, nummer2):
+    return nummer1 * nummer2
+
+def delen(nummer1, nummer2):
+    if nummer2 == 0:
+        return "Error: kan niet delen door 0!"
+    return nummer1 / nummer2
