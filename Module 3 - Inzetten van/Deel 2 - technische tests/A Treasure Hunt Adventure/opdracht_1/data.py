65a4e700a3a5c0efc1d30d81f1336dfabed271c4
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-10-04 09:20:54 +0200

    opdracht gemaakt

---
+++ Module 3 - Inzetten van/Deel 2 - technische tests/A Treasure Hunt Adventure/opdracht_1/data.py
@@ -0,0 +1,25 @@
+#constants
+JOURNEY_IN_DAYS = 0
+name = input('What is your name? ')
+
+#data
+mainCharacter = {
+    #'name' : '',
+    name : name,
+    'ownsHorse' : True,
+    'adventuring' : True,
+    'cash' : {
+        'platinum' : 0,
+        'gold' : 1,
+        'silver' : 7,
+        'copper' : 5
+    }
+}
+
+friends = []
+
+adventurerGear = []
+
+investors = []
+
+treasure = []
