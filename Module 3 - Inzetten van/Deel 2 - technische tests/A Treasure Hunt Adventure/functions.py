2bd5b1cf9d14f1d5644009ddd303eb4e80d1a5cb
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-10-10 09:23:41 +0200

    bijgewerkt

--- Module 3 - Inzetten van/Deel 2 - technische tests/A Treasure Hunt Adventure/functions.py
+++ Module 3 - Inzetten van/Deel 2 - technische tests/A Treasure Hunt Adventure/functions.py
@@ -50,6 +50,12 @@ def getAdventuringFriends(friends:list) -> list:
     return getFromListByKeyIs(friends, 'adventuring', True)
 
 ##################### O07 #####################
+#Geen tenten en 1 paard, ja dat is inderdaad niet heel handig als je op een avontuur van 11 dagen gaat. 
+#Na wat navragen in het dorp hebben ze op de volgende informatie vergaard: 
+#In de tenten (die ze kunnen huren) kunnen max 3 personen slapen en op ieder paard kunnen 2 personen rijden. 
+#Een paard huren kost 5 zilver per dag en een tent huren kost 3 goud per week ongeacht hoeveel dagen je hem die week gebruikt.
+#Zorg weer voor een errorloze applicatie na het invullen van de volgende 3 functies: 
+#getNumberOfHorsesNeeded, getNumberOfTentsNeeded en getTotalRentalCost.
 #Voor deze functies heb je de library math nodig met de functie ceil.
 
 def getNumberOfHorsesNeeded(people:int) -> int:
@@ -59,8 +65,10 @@ def getNumberOfTentsNeeded(people:int) -> int:
     return math.ceil(people / 3)
 
 def getTotalRentalCost(horses:int, tents:int) -> float:
-    # totaal in float
-    return COST_HORSE_SILVER_PER_DAY * horses * JOURNEY_IN_DAYS / 5 + COST_TENT_GOLD_PER_WEEK * tents
+    #met de hulp van de 003 functions, de nieuwe constanten en JOURNEY_IN_DAYS.
+    # totaal in float omdat er goud en zilver in zit.
+    return (horses * COST_HORSE_SILVER_PER_DAY + tents * COST_TENT_GOLD_PER_WEEK) * JOURNEY_IN_DAYS / 5 # 5 zilver = 1 goud
+    #return (horses * COST_HORSE_SILVER_PER_DAY + tents * COST_TENT_GOLD_PER_WEEK) * JOURNEY_IN_DAYS / 5 # 5 zilver = 1 goud
 
 ##################### O08 #####################
 
