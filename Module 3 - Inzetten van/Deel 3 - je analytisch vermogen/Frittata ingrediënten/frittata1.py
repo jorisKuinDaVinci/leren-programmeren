e8deb27ba58c8c413e7b318b30b3911832372fb8
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-11-11 11:28:31 +0100

    eerste opdracht goed afgerond

--- Module 3 - Inzetten van/Deel 3 - je analytisch vermogen/Frittata ingrediënten/frittata1.py
+++ Module 3 - Inzetten van/Deel 3 - je analytisch vermogen/Frittata ingrediënten/frittata1.py
@@ -36,38 +36,48 @@ nr_persons = input_nr_persons('Voor hoeveel personen wilt u frittata maken? ')
 calc_factor = nr_persons / 4 # 4 persons is the base of the recipe
 
 # calculate amount_eggs
-
+calc_amount_eggs = AMOUNT_EGGS * calc_factor
 
 # calculate amount_milk
-
+calc_amount_milk = AMOUNT_MILK * calc_factor
 
 # calculate amount_salt
-
+calc_amount_salt = AMOUNT_SALT * calc_factor
 
 # calculate amount_pepper
-
+calc_amount_pepper = AMOUNT_PEPPER * calc_factor
 
 # calculate amount_oil
-
+calc_amount_oil = AMOUNT_OIL * calc_factor
 
 # calculate amount_onions
-
+calc_amount_onions = AMOUNT_ONIONS * calc_factor
 
 # calculate amount_garlics
-
+calc_amount_garlics = AMOUNT_GARLICS * calc_factor
 
 # calculate amount_spinach
-
+calc_amount_spinach = AMOUNT_SPINACH * calc_factor
 
 # calculate amount_paprikas
-
+calc_amount_paprikas = AMOUNT_PAPRIKAS * calc_factor
 
 # calculate amount_cheese
-
+calc_amount_cheese = AMOUNT_CHEESE * calc_factor
 
 # -------- OUTPUT -------
 print('=============== Frittata recept ===============')
 print(f'Ingrediënten voor {nr_persons} personen:')
 print('-----------------------------------------------')
 # print (formatted) all amounts and units combined with their ingrediënt descriptions
+print(f'{calc_amount_eggs} {UNIT_EGGS} {TXT_EGGS}')
+print(f'{calc_amount_milk} {UNIT_MILK} {TXT_MILK}')
+print(f'{calc_amount_salt} {UNIT_SALT} {TXT_SALT}')
+print(f'{calc_amount_pepper} {UNIT_PEPPER} {TXT_PEPPER}')
+print(f'{calc_amount_oil} {UNIT_OIL} {TXT_OIL}')
+print(f'{calc_amount_onions} {UNIT_ONIONS} {TXT_ONIONS}')
+print(f'{calc_amount_garlics} {UNIT_GARLICS} {TXT_GARLICS}')
+print(f'{calc_amount_spinach} {UNIT_SPINACH} {TXT_SPINACH}')
+print(f'{calc_amount_paprikas} {UNIT_PAPRIKAS} {TXT_PAPRIKAS}')
+print(f'{calc_amount_cheese} {UNIT_CHEESE} {TXT_CHEESE}')
 print('-----------------------------------------------')
