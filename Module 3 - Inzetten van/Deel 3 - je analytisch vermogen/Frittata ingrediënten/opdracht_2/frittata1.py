b0b12fc5a5820a11862e20e13c3e051c193bf4df
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-11-11 11:39:47 +0100

    uitleg opdracht 2

--- Module 3 - Inzetten van/Deel 3 - je analytisch vermogen/Frittata ingrediënten/opdracht_2/frittata1.py
+++ Module 3 - Inzetten van/Deel 3 - je analytisch vermogen/Frittata ingrediënten/opdracht_2/frittata1.py
@@ -31,6 +31,38 @@ nr_persons = input_nr_persons('Voor hoeveel personen wilt u frittata maken? ')
 # spinach: 67.5; 
 # cheese: 11.25
 
+#Heb je dezelfde uitkomsten? Prima, dan ga je de afrondingen berekenen afhankelijk van gebruikte eenheid.
+#De gebruikte eenheden van ingrediënten vind je in frittata_ingredients.py, 
+# bijvoorbeeld voor melk is het UNIT_MILK = UNIT_CUPS, of voor peper is  het UNIT_PEPPER = UNIT_TEASPOONS
+#Codeer in frittata1.py de afronding van hoeveelheden van alle ingrediënten afhankelijk van de hoeveelheid en van de eenheid:
+#als eenheid gelijk is aan  pieces (UNIT_PIECES):
+#gebruik function round_piece() in recipe_lib\
+#maak die function werkend:
+#meegekregen hoeveelheid (amount) naar boven afronden naar een geheel getal
+#return het resultaat van de afronding
+#als eenheid gelijk is aan spoons, teaspoons of cups (UNIT_SPOONS, UNIT_TEASPOONS, UNIT_CUPS):
+#gebruik function round_quarter() in recipe_lib
+#maak de function werkend:
+#meegekregen hoeveelheid (amount) afronden naar het dichtsbijzijnde getal met: .25 of .50 of .75 of .00.
+#meegekregen hoeveelheid boven de 10 afronden naar het dichtstbijzijnde gehele getal.
+#return het resultaat van de afronding
+#tip: kijk naar de berekening in: Real simple functions, opdracht: Afronden naar stuivers. Nu afronden naar 25 ipv 5
+#Codeer het printen van de hoeveelheden de eenheden in combinatie met de omschrijving van de ingrediënten. 
+#codeer het totdat de bovenstaande voorbeeld output ontstaat bij een invoer van 13 personen
+#gebruik de berekenende hoeveelheden
+#gebruik de TXT_ en de UNIT_ constanten in de py-code) bijvoorbeeld: TEXT_MILK of UNIT_MILK
+#gebruik de typische python string formatting, bijvoorbeeld f'…….’
+#nog niets doen aan het presenteren van omschrijvingen in enkelvoud of meervoud (dat komt later!)
+#Test het programma voor de invoer van resp.: 1, 3, 7, 13 en 90 personen.
+#Vergelijk de ouputs van de testen met de getallen in onderstaande tabel:
+
+#personen	ei	melk	zout	peper	olie	ui	knoflook	paprika	spinazie	kaas
+#1	        2	0.25	0.25	0.25	0.25	1	1	        1	    0.25	    0.25
+#3	        6	0.5	    0.5	    0.5	    0.75	1	2	        3	    0.75	    0.5
+#7	        13	1.0	    1.0	    1.0	    1.75	2	4	        6	    1.75	    1.0
+#13	        23	1.5	    1.5	    1.5	    3.25	4	7	        10	    3.25	    1.5
+#90	        158	11	    11	    11	    22	    23	45	        68	    22	        11
+
 # ----- CALCULATIONS ----
 # calculate factor 
 calc_factor = nr_persons / 4 # 4 persons is the base of the recipe
