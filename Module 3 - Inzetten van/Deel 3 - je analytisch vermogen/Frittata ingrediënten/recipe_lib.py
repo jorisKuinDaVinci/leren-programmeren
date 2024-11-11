79da71f4a682dad0e38230d5247cc92eed47115f
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-11-11 11:22:41 +0100

    bijgewerkt

--- Module 3 - Inzetten van/Deel 3 - je analytisch vermogen/Frittata ingrediënten/recipe_lib.py
+++ Module 3 - Inzetten van/Deel 3 - je analytisch vermogen/Frittata ingrediënten/recipe_lib.py
@@ -23,33 +23,22 @@ def input_nr_persons(prompt: str) -> int:
 
 
 def round_piece(amount: float) -> int:
-  return math.ceil(amount)
+  pass
 
 # returns amount rounded to the closest decimals: .00 or .25 or .50 or 0.75 unless amount >= 10
 def round_quarter(amount: float) -> float:
-  if amount >= 10:
-    return round(amount)
-  return round(amount * 4) / 4
+  pass
 
 
 # returns single or plural description of a string 'single desciption|plural description' 
 # depending on amount
 def str_single_plural(amount: float, txt: str) -> str:
-  if amount == 1:
-    return txt.split('|')[0]
-  return txt.split('|')[1]
+  pass
 
 
 # returns description of single or plural units
 def str_units(amount: float, unit: str) -> str:
-  if unit == UNIT_PIECES:
-    return str_single_plural(amount, TXT_PIECES)
-  if unit == UNIT_SPOONS:
-    return str_single_plural(amount, TXT_SPOONS)
-  if unit == UNIT_TEASPOONS:
-    return str_single_plural(amount, TXT_TEASPOONS)
-  if unit == UNIT_CUPS:
-    return str_single_plural(amount, TXT_CUPS)
+  pass
 
 
 # returns amount in string with 1/4 or 1/2 or 3/4
@@ -68,13 +57,7 @@ ML_TEASPOON = 5 # one teaspoon contains 5 ml
 ML_CUP = 240 # one cup contains 240 ml
 
 def unit2ml(amount: float, unit: str) -> float:
-  if unit == UNIT_SPOONS:
-    return amount * ML_SPOON
-  if unit == UNIT_TEASPOONS:
-    return amount * ML_TEASPOON
-  if unit == UNIT_CUPS:
-    return amount * ML_CUP
-  return amount
+  pass
 
 # average densities in gram per ml for common ingredients, to calculate weight(gram) from milliliters(ml)
 # 1ml of salt weighs 1.2 gram 
@@ -85,4 +68,5 @@ GRAM_PER_ML_SPINACH = 0.15
 
 # returns amount in gram for amount in milliliter based on density (weight per volume)
 def ml2gram(amount_ml: float, gram_per_ml: float) -> float:
-  return amount_ml * gram_per_ml
+  pass
+
