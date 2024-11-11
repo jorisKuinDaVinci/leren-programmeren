1fcce9ecaeab6efd8f329bfd7b00a69622a2d137
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-11-11 11:05:35 +0100

    bijgewerkt

--- Module 3 - Inzetten van/Deel 3 - je analytisch vermogen/Frittata ingrediënten/frittata1.py
+++ Module 3 - Inzetten van/Deel 3 - je analytisch vermogen/Frittata ingrediënten/frittata1.py
@@ -5,34 +5,46 @@ from frittata_ingredients import *
 print('=============== Frittata recept ===============')
 # -------- INPUT --------
 # use recipe_lib for input of nr_persons
-nr_persons = int(input()) # replace this with better input
+nr_persons = input_nr_persons('Voor hoeveel personen wilt u frittata maken? ')
 
 # ----- CALCULATIONS ----
 # calculate factor 
+calc_factor = nr_persons / 4 # 4 persons is the base of the recipe
 
 # calculate amount_eggs
+calc_amount_eggs = round_piece(4 * calc_factor)
 
 # calculate amount_milk
+calc_amount_milk = round_quarter(0.5 * calc_factor)
 
 # calculate amount_salt
+calc_amount_salt = round_quarter(0.5 * calc_factor)
 
 # calculate amount_pepper
+calc_amount_pepper = round_quarter(0.5 * calc_factor)
 
 # calculate amount_oil
+calc_amount_oil = round_quarter(1 * calc_factor)
 
 # calculate amount_onions
+calc_amount_onions = round_quarter(1 * calc_factor)
 
 # calculate amount_garlics
+calc_amount_garlics = round_quarter(1 * calc_factor)
 
 # calculate amount_spinach
+calc_amount_spinach = round_quarter(400 * calc_factor)
 
 # calculate amount_paprikas
+calc_amount_paprikas = round_quarter(2 * calc_factor)
 
 # calculate amount_cheese
+calc_amount_cheese = round_quarter(100 * calc_factor)
 
 # -------- OUTPUT -------
 print('=============== Frittata recept ===============')
 print(f'Ingrediënten voor {nr_persons} personen:')
 print('-----------------------------------------------')
 # print (formatted) all amounts and units combined with their ingrediënt descriptions
+print(f'{calc_amount_eggs} {str_units(calc_amount_eggs, UNIT_PIECES)}')
 print('-----------------------------------------------')
