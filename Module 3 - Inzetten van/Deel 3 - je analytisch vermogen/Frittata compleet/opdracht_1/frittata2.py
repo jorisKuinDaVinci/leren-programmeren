4639048d911948bd33760643b306c3e42a985542
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-11-13 10:25:30 +0100

    bijgewerkt

--- Module 3 - Inzetten van/Deel 3 - je analytisch vermogen/Frittata compleet/opdracht_1/frittata2.py
+++ Module 3 - Inzetten van/Deel 3 - je analytisch vermogen/Frittata compleet/opdracht_1/frittata2.py
@@ -7,6 +7,28 @@ print('=============== Frittata recept ===============')
 # Gebruik recipe_lib voor input van aantal personen
 nr_persons = input_nr_persons('Voor hoeveel personen wilt u frittata maken? ')
 
+#Kan netter!
+#De app geeft nu een resultaat met enkele minpuntjes.
+
+#omschrijvingen worden getoond in enkelvoud én in meervoud, zoals “persoon|personen” of “groot ei|grote eieren” (roodomcirkeld)!  Dat is nogal twijfelachtig! We willen voor 1 ei zien: 1 groot ei. En voor 23 eieren: 23 grote eieren.
+#de hoeveelheden voor lepels, theelepels en kopjes worden niet getoond zoals gebruikelijk (geelomcirkeld)! Voor bijvoorbeeld: 3.25,  1.5, 0.75 en 4.0 willen we zien: 3¼ , 1½ , ¾ en 4
+#de eenheden voor lepels, theelepels en kopjes worden getoond in het Engels (groenomcirkeld)! In plaats van 1.5 teaspoon: 1½ lepel en in plaats van 3.25 cup: 3¼ kopjes. Dus Nederlandse namen en dan in enkel- of meervoud afhankelijk van de hoeveelheid. (Alles vanaf de 2 dus in meervoud!)
+
+#Stappenplan
+#Codeer in frittata2.py de enkelvoud of meervoud ingrediënt-omschrijvingen afhankelijk van de hoeveelheid.
+#gebruik de function str_single_plural()
+#maak de function wel eerst werkend:
+#meervoud vanaf hoeveelheid 2
+#hoe kan je een string splitsen in twee delen, waarvan je er een kan returnen?
+#Codeer het presenteren van getallen met ¼ , ½  en ¾ (function uit recipe_lib.py?)
+#gebruik de function str_amount_fraction() uit recipe_lib.py. Deze lastige function is al werkend gemaakt.
+#Codeer het presenteren van de juiste eenheden voor spoons, teaspoons en cups.
+#gebruik de function str_units()
+#maak de function wel eerst werkend:
+#maak hierin gebruik van de function str_single_prural()
+#maak hierin ook gebruik van de constanten: TXT_SPOONS, TXT_TEASPOONS en TXT_CUPS
+#Test de werking van frittata2 met verschillende testgevallen.
+
 # ----- CALCULATIONS ----
 # calculate factor 
 calc_factor = nr_persons / RECIPE_PERSONS  # 4 personen is de basis van het recept
