4f4a2bf2447b6de902d4276d1ed5841c5c2a1fac
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-11-20 11:57:04 +0100

    bijgewerkt

---
+++ Module 3 - Inzetten van/Deel 4 - je verbeterde kennis/Lootjes verdeler update/backup/lootjes_verdeler_code.py
@@ -0,0 +1,32 @@
+from random import shuffle
+
+def geef_naam(namen):
+    while True:
+        naam = input("Geef een naam: ").lower()
+        if naam in namen:
+            print("Deze naam is al ingevoerd, probeer een andere naam.")
+        else:
+            return naam
+
+namen = []
+print(type(namen))
+
+while True:
+    naam = geef_naam(namen)
+    namen.append(naam)
+    if len(namen) >= 3 and input("Wil je nog een naam invoeren? ").lower() != "ja":
+        break
+
+lootjes = namen.copy()
+
+
+while True:
+    fout = False
+    shuffle(lootjes)
+    for i in range(len(namen)):
+        if namen[i] == lootjes[i]:
+            shuffle(lootjes)
+            fout = True
+            break
+    if not fout:
+        break
