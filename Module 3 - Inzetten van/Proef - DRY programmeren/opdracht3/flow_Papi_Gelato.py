b97d553d99c27650543221c0c914a34efa7b61d7
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-12-18 09:18:04 +0100

    bijgewerkt

--- Module 3 - Inzetten van/Proef - DRY programmeren/opdracht3/flow_Papi_Gelato.py
+++ Module 3 - Inzetten van/Proef - DRY programmeren/opdracht3/flow_Papi_Gelato.py
@@ -1,4 +1,4 @@
-from functions_Papi_Gelato import vraag_aantal_bolletjes, controleer_aantal_bolletjes, vraag_hoorntje_of_bakje,controleer_keuze, bevestiging_bestelling, vraag_nog_meer, controleer_meer_bestellen, print_bon
+from functions_Papi_Gelato import vraag_aantal_bolletjes, controleer_aantal_bolletjes, vraag_smaak, controleer_smaak, vraag_hoorntje_of_bakje, controleer_keuze, bevestiging_bestelling, vraag_nog_meer, controleer_meer_bestellen, print_bon
 from data_Papi_Gelato import TEKSTEN
 
 print(TEKSTEN["welkom_tekst"])
@@ -8,6 +8,12 @@ bestellingen = {
     "bolletjes": 0,
     "hoorntjes": 0,
     "bakjes": 0,
+    "smaken": {
+        "Aardbei": 0,
+        "Chocolade": 0,
+        "Munt": 0,
+        "Vanille": 0,
+    },
 }
 
 while True:
@@ -16,6 +22,19 @@ while True:
     status, aantal = controleer_aantal_bolletjes(aantal_input)
 
     if status == "stap2":
+        smaken = []
+        for i in range(1, aantal + 1):
+            while True:
+                print(vraag_smaak(i))
+                smaak = input("> ")
+                gekozen_smaak = controleer_smaak(smaak)
+                if gekozen_smaak:
+                    smaken.append(gekozen_smaak)
+                    bestellingen["smaken"][gekozen_smaak] += 1
+                    break
+                else:
+                    print("Sorry dat snap ik niet...")
+
         while True:
             print(vraag_hoorntje_of_bakje(aantal))
             keuze = input("> ")
@@ -31,6 +50,18 @@ while True:
             else:
                 print("Sorry, dat snap ik niet...")
     elif status == "bakje":
+        smaken = []
+        for i in range(1, aantal + 1):
+            while True:
+                print(vraag_smaak(i))
+                smaak = input("> ")
+                gekozen_smaak = controleer_smaak(smaak)
+                if gekozen_smaak:
+                    smaken.append(gekozen_smaak)
+                    bestellingen["smaken"][gekozen_smaak] += 1
+                    break
+                else:
+                    print("Sorry dat snap ik niet...")
         print(f"Dan krijgt u van mij een bakje met {aantal} bolletjes.")
         bestellingen["bolletjes"] += aantal
         bestellingen["bakjes"] += 1
--- Module 3 - Inzetten van/Proef - DRY programmeren/opdracht3/functions_Papi_Gelato.py
+++ Module 3 - Inzetten van/Proef - DRY programmeren/opdracht3/functions_Papi_Gelato.py
@@ -15,6 +15,13 @@ def controleer_aantal_bolletjes(aantal):
     except ValueError:
         return "ongeldig", None
 
+def vraag_smaak(bolletje_nummer):
+    return f"Welke smaak wilt u voor bolletje nummer {bolletje_nummer}? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?"
+
+def controleer_smaak(keuze):
+    smaken = {"A": "Aardbei", "C": "Chocolade", "M": "Munt", "V": "Vanille"}
+    return smaken.get(keuze.upper(), None)
+
 def vraag_hoorntje_of_bakje(aantal):
     return f"Wilt u deze {aantal} bolletje(s) in een hoorntje of een bakje?"
 
@@ -42,10 +49,11 @@ def print_bon(bestellingen):
     print("\n---------[\"Papi Gelato\"]---------")
     totaal = 0.0
 
-    if bestellingen["bolletjes"] > 0:
-        prijs = bestellingen["bolletjes"] * 1.10
-        totaal += prijs
-        print(f"Bolletjes {bestellingen['bolletjes']:>3} x €1.10 = €{prijs:6.2f}")
+    for smaak, aantal in bestellingen["smaken"].items():
+        if aantal > 0:
+            prijs = aantal * 1.10
+            totaal += prijs
+            print(f"{smaak:<9} {aantal:>3} x €1.10 = €{prijs:6.2f}")
 
     if bestellingen["hoorntjes"] > 0:
         prijs = bestellingen["hoorntjes"] * 1.25
