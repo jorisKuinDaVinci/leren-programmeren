274644f12a1aa24a299d8d79cb514b9a3dee7e81
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-12-18 09:39:44 +0100

    bijgewerkt

--- Module 3 - Inzetten van/Proef - DRY programmeren/opdracht3/data_Papi_Gelato.py
+++ Module 3 - Inzetten van/Proef - DRY programmeren/opdracht3/data_Papi_Gelato.py
@@ -1,4 +1,4 @@
 # Algemene teksten
 TEKSTEN = {
-    "welkom_tekst": "Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar Aardbei, Chocolade, Munt of Vanille ijs is.",
+    "welkom_tekst": "Welkom bij Papi Gelato.",
 }
