5365b2f59f19e12a846009943e524a582aa99516
jorisKuinDaVinci <99074486@mydavinci.nl>
2025-01-06 11:30:31 +0100

    bijgewerkt

--- Module 3 - Inzetten van/Proef - DRY programmeren/opdracht5/data_Papi_Gelato.py
+++ Module 3 - Inzetten van/Proef - DRY programmeren/opdracht5/data_Papi_Gelato.py
@@ -1,4 +1,41 @@
-# Algemene teksten
+# Algemene teksten voor de Papi Gelato applicatie
 TEKSTEN = {
+    # Welkomstbericht
     "welkom_tekst": "Welkom bij Papi Gelato.",
+
+    # Klanttype vragen
+    "vraag_klant_type": "Bent u 1) een particuliere klant of 2) een zakelijke klant?",
+
+    # Ongeldige invoer
+    "ongeldige_invoer": "Sorry, dat snap ik niet...",
+
+    # Teveel bolletjes
+    "teveel_bolletjes": "Sorry, zulke grote bakken hebben we niet.",
+
+    # Vraag naar smaken
+    "vraag_smaak": "Welke smaak wilt u voor bolletje nummer {bolletje_nummer}? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?",
+
+    # Vraag naar hoorntje of bakje
+    "vraag_hoorntje_of_bakje": "Wilt u deze {aantal} bolletje(s) in een hoorntje of een bakje?",
+
+    # Bevestiging van de bestelling
+    "bevestiging_bestelling": "Hier is uw {keuze} met {aantal} bolletje(s).",
+
+    # Vraag naar toppings
+    "vraag_topping": "Wat voor topping wilt u: A) Geen, B) Slagroom, C) Sprinkels of D) Caramel Saus?",
+
+    # Vraag om meer te bestellen
+    "vraag_nog_meer": "Wilt u nog meer bestellen?",
+
+    # Afsluiting
+    "afsluit_tekst": "Bedankt en tot ziens!",
+
+    # Vraag naar liters voor zakelijke klanten
+    "vraag_liters": "Hoeveel liter wilt u?",
+
+    # Vraag naar smaak voor zakelijke klanten
+    "vraag_smaak_liter": "Welke smaak wilt u voor liter nummer {liter_nummer}? A) Aardbei, C) Chocolade, M) Munt of V) Vanille?",
+
+    # Foutmelding bij negatief of ongeldig aantal liters
+    "ongeldig_aantal_liters": "Voer een geldig aantal liters in.",
 }
