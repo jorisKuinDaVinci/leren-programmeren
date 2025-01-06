d5776f996bc9c9117683ba09162266b741bb5c14
jorisKuinDaVinci <99074486@mydavinci.nl>
2025-01-06 11:27:44 +0100

    bijgewerkt

--- Module 3 - Inzetten van/Proef - DRY programmeren/opdracht5/flow_Papi_Gelato.py
+++ Module 3 - Inzetten van/Proef - DRY programmeren/opdracht5/flow_Papi_Gelato.py
@@ -1,4 +1,4 @@
-from functions_Papi_Gelato import (
+from functions_Papi_Gelato import ( 
     vraag_aantal_bolletjes,
     controleer_aantal_bolletjes,
     vraag_smaak,
@@ -19,47 +19,55 @@ from functions_Papi_Gelato import (
 )
 from data_Papi_Gelato import TEKSTEN
 
-print(TEKSTEN["welkom_tekst"])
+# Functie om bestellingen te resetten
+def reset_bestellingen():
+    return {
+        "bolletjes": 0,
+        "hoorntjes": 0,
+        "bakjes": 0,
+        "smaken": {
+            "Aardbei": 0,
+            "Chocolade": 0,
+            "Munt": 0,
+            "Vanille": 0,
+        },
+        "topping_prijs": 0.0,
+    }
 
-# Bestellingen bijhouden
-bestellingen = {
-    "bolletjes": 0,
-    "hoorntjes": 0,
-    "bakjes": 0,
-    "smaken": {
-        "Aardbei": 0,
-        "Chocolade": 0,
-        "Munt": 0,
-        "Vanille": 0,
-    },
-    "topping_prijs": 0.0,
-}
+# Start van het programma
+print(TEKSTEN.get("welkom_tekst", "Welkom bij Papi Gelato!"))
 
+# Hoofdprogramma
 while True:
+    bestellingen = reset_bestellingen()
+
     # Vraag klanttype
     while True:
         print(vraag_klant_type())
-        klant_type = input("> ")
+        klant_type = input("> ").strip()
         klant_type = controleer_klant_type(klant_type)
         if klant_type:
             break
         else:
-            print("Sorry dat snap ik niet...")
+            print("Sorry dat snap ik niet. Kies 1 voor particulier of 2 voor zakelijk.")
 
+    # Behandel zakelijke of particuliere klant
     if klant_type == "zakelijk":
         zakelijke_klant_workflow(bestellingen)
+        continue  # Start opnieuw voor de volgende klant
     elif klant_type == "particulier":
         particulier_workflow(bestellingen)
 
-    # Vraag of de klant nog meer wil bestellen
+    # Vraag of de klant meer wil bestellen
     while True:
         print(vraag_nog_meer())
-        meer_input = input("> ")
+        meer_input = input("> ").strip()
         meer = controleer_meer_bestellen(meer_input)
         if meer is True:
             break  # Start een nieuwe bestelling
         elif meer is False:
             print_bon(bestellingen)  # Toon de bon en sluit af
+            print("Tot ziens!")
             exit()
         else:
             print("Sorry, dat snap ik niet...")
